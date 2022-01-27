fae391e6728d2d1713274da35887270e0899ad14
SoulOfLadros <sebastian.arayat@sansano.usm.cl>
2022-01-27 17:56:55 -0300

    casi listo

--- Main_end/handler.py
+++ Main_end/handler.py
@@ -4,6 +4,7 @@ from fpdf import FPDF
 import json
 import boto3
 from botocore.client import Config
+import datetime
 
 
 def llamada(event, context):
@@ -43,3 +44,58 @@ def llamada(event, context):
     
     return response
 
+
+def NuevoDolar(event,context):
+    client = boto3.resource('dynamodb', aws_access_key_id="S3RVER",aws_secret_access_key="S3VER",endpoint_url='http://localhost:8000')
+    
+    #check if table exist
+
+    lista_tablas = [table.name for table in client.tables.all()]
+    
+    if 'DolarDia' not in lista_tablas:
+        tabla = client.create_table(
+        TableName='DolarDia',
+        KeySchema=[
+            {
+                'AttributeName': 'fecha',
+                'KeyType': 'HASH'  
+            },
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'fecha',
+                'AttributeType': 'S'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+    
+    tabla = client.Table('DolarDia')
+
+    dia_actual = datetime.datetime.today().strftime('%d-%m-%Y')
+    hora_actual = datetime.datetime.today().strftime('%H:%M')
+
+    valores = get("https://mindicador.cl/api/dolar/"+dia_actual)
+    dic_valores = json.loads(valores.text)
+
+    val_dolar = str(dic_valores['serie'][0]['valor'])
+
+    response = tabla.put_item(Item = { 'fecha':dia_actual+hora_actual,'valor': val_dolar })
+
+    response = {"statusCode": 200, "body": valores.text}
+    
+    return response
+
+def TestingData (event,context):
+    client = boto3.client('dynamodb', aws_access_key_id="S3RVER",aws_secret_access_key="S3VER",endpoint_url='http://localhost:8000')
+    
+    response = client.scan(TableName='DolarDia')
+
+
+    response = {"statusCode": 200, "body": json.dumps(response)}
+    
+    return response
